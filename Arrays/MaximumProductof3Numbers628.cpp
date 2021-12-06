3f9fdc1429fea85dff8b71eb53ca6c26e9b6a10e
Devangi Purkayastha <54393816+devangi2000@users.noreply.github.com>
2021-12-06 08:41:16 +0000

    ...

---
+++ Arrays/MaximumProductof3Numbers628.cpp
@@ -0,0 +1,48 @@
+// Given an integer array nums, find three numbers whose product is maximum and return the maximum product.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3]
+// Output: 6
+// Example 2:
+
+// Input: nums = [1,2,3,4]
+// Output: 24
+// Example 3:
+
+// Input: nums = [-1,-2,-3]
+// Output: -6
+ 
+
+// Constraints:
+
+// 3 <= nums.length <= 104
+// -1000 <= nums[i] <= 1000
+
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        int min1 = INT_MAX, min2 = INT_MIN, max1 = INT_MIN, max2 = INT_MIN, max3 = INT_MIN;
+        for(int num : nums){
+            if(num < min1){
+                min2 = min1;
+                min1 = num;
+            } else if(num < min2){
+                min2 = num;
+            }
+            if(num > max1){
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            } else if(num > max2){
+                max3 = max2;
+                max2 = num;
+            } else if(num > max3){
+                max3 = num;
+            }
+        }
+        return max(min1 * min2 * max1, max1 * max2 * max3);
+    }
+};
